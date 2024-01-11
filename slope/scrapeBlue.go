86e540455934aa532f02fd6337c343dc40de4f94
Lukas Belashov <lukasbelashov@gmail.com>
2024-01-11 16:10:50 -0500

    fixed bug

--- slope/scrapeBlue.go
+++ slope/scrapeBlue.go
@@ -3,44 +3,48 @@ package slope
 import (
 	"fmt"
 	"github.com/gocolly/colly"
+	"sync"
 )
 
-func ScrapeBlueMountain() (*Conditions, error) {
+//1-11-2024: Implemented a wait group to account for data collection due to the asynchronous nature of web scraping
+
+func ScrapeBlueMountain() (Conditions, error) {
 	URL := "https://www.skibluemt.com/condition-report/"
 
-	conditions := &Conditions{}
+	conditions := Conditions{}
 	conditionList := []string{}
+	var wg sync.WaitGroup
 
 	c := colly.NewCollector()
 	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
 	c.OnRequest(func(request *colly.Request) {
 		fmt.Println("Visiting:", request.URL)
+		wg.Add(1)
 	})
 
 	c.OnResponse(func(response *colly.Response) {
 		fmt.Println("Status:", response.StatusCode)
+		wg.Done()
 	})
 
 	c.OnHTML("p.has-text-align-center", func(element *colly.HTMLElement) {
 		conditionList = append(conditionList, element.Text)
-		fmt.Println(conditionList)
-
 	})
-
-	fmt.Println(conditionList)
-
-	//conditions.Date = conditionList[0]
-	//conditions.BaseDepth = conditionList[1]
-	//conditions.LiftsOperating = conditionList[2]
-	//conditions.OpenTrails = conditionList[3]
-	//conditions.TerrainParks = conditionList[4]
-	//conditions.Tubing = conditionList[5]
 
 	err := c.Visit(URL)
 	if err != nil {
 		return conditions, err
 	}
+
+	conditions.Date = conditionList[0]
+	conditions.BaseDepth = conditionList[1]
+	conditions.LiftsOperating = conditionList[2]
+	conditions.OpenTrails = conditionList[3]
+	conditions.TerrainParks = conditionList[4]
+	conditions.Tubing = conditionList[5]
+
+	wg.Wait()
 
 	return conditions, err
 }
